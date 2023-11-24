c2a14aeafb68af2a452b3846f198541bb23fede4
Dennis Dobirr <me@dennisdobirr.de>
2023-11-24 12:09:38 +0100

    more routing

---
+++ app/(dashboard)/auth/[...sign-in]/page.js
@@ -0,0 +1,12 @@
+
+
+const signInPage = ({params}) => {
+    console.log(params);
+    return (
+        <div>
+            <h1 className="text-5xl mb-8 font-bold">SignInPAge</h1>
+        </div>
+    );
+};
+
+export default signInPage;
---
+++ app/(dashboard)/auth/page.js
@@ -0,0 +1,11 @@
+
+
+const AuthPage = () => {
+    return (
+        <div>
+            <h1 className="text-5xl mb-8 font-bold">Authpage</h1>
+        </div>
+    );
+};
+
+export default AuthPage;
